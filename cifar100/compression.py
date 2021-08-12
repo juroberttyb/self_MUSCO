bf0f4e0859697ea245f271f08d5c9ba0b3c605b0
juroberttyb <juroberttyb@gmail.com>
2021-08-13 00:02:38 +0800

    sensitive check done, not tested though

--- cifar100/compression.py
+++ cifar100/compression.py
@@ -136,7 +136,7 @@ def Res50_MUSCO_approach():
         # bl.validation(net, val_loader, criterion)
         table = sensitive_check(net.cpu(), reduction_rate, val_loader, criterion)
         print("acc after sensitive check")
-        bl.validation(net, val_loader, criterion)
+        bl.validation(net.cuda(), val_loader, criterion)
         net = resnet_MuscoStep(net.cpu(), table, reduction_rate).cuda()
         info(net)
         # print(net)
